f2dd49c8ec7b7b21e992846b033bab6bf035ea1a
Haritakari <davidsanchezdevesa@gmail.com>
2018-06-18 00:43:05 +0200

    Encapsulate logic in entity 'item'

--- src/Item.php
+++ src/Item.php
@@ -8,6 +8,9 @@ class Item {
     private $sellIn;
     private $quality;
 
+    const AGED_BRIE = 'Aged Brie';
+    const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+
     function __construct(string $name, int $sellIn, int $quality) {
         $this->name = $name;
         $this->sellIn = $sellIn;
@@ -42,6 +45,38 @@ class Item {
 
         return $this;
     }
+    public function isSellIsNegative()
+    {
+        if ($this->getSellIn() < 0) {
+            if ($this->getName() != self::AGED_BRIE) {
+                if ($this->getName() != self::BACKSTAGE) {
+                    $this->degradeQuality();
+                } else {
+                    $this->setQuality(0);
+                }
+            } else {
+                $this->upgrateQuality();
+            }
+        }
+    }
+
+    public function changeQuality()
+    {
+        if ($this->getName() != self::AGED_BRIE and $this->getName() != self::BACKSTAGE) {
+            $this->degradeQuality($this);
+        } else {
+            $this->upgrateQuality($this);
+
+            if ($this->getName() == self::BACKSTAGE) {
+                if ($this->getSellIn() < 11) {
+                    $this->upgrateQuality($this);
+                }
+                if ($this->getSellIn() < 6) {
+                    $this->upgrateQuality($this);
+                }
+            }
+        }
+    }
 
     public function degradeQuality()
     {
@@ -50,10 +85,10 @@ class Item {
         }
     }
 
-    public function upgrateQuality($item)
+    public function upgrateQuality()
     {
         if ($this->getQuality() < 50) {
-            $this->setQuality($item->getQuality() + 1);
+            $this->setQuality($this->getQuality() + 1);
         }
     }
 }
